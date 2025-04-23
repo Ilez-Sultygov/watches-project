3184a51d5889b9cb84b191dd69abc723fbb710c7
GlebUrv <punkhoygleb@mail.ru>
2025-04-23 17:41:19 +0500

    auth feature (no cookier)

--- client/src/pages/auth/forms/SignUpForm.jsx
+++ client/src/pages/auth/forms/SignUpForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import UserValidator from "../../../../entities/user/User.validator";
-import UserApi from "../../../../entities/user/UserApi";
+import UserValidator from "../../../entities/user/User.validator";
+import UserApi from "../../../entities/user/UserApi";
 import { useNavigate } from "react-router";
 
 const INITIAL_INPUTS_DATA = {
@@ -18,5 +18,73 @@ export default function SignUpForm({ setUser }) {
     setInputs((prev) => ({ ...prev, [event.target.name]: event.target.value }));
   };
 
-  const onSubmitHandler = async (event) =>
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+    const { isValid, error } = UserValidator.validateSignUp(inputs);
+
+    if (!isValid) return alert(error);
+
+    try {
+      const {
+        statusCode,
+        data,
+        error: responseError,
+      } = await UserApi.signUp(inputs);
+
+      if (responseError) {
+        alert(responseError);
+        return;
+      }
+
+      if (statusCode === 201) {
+        setUser(data.user);
+        setInputs(INITIAL_INPUTS_DATA);
+        navigate("/watches");
+      }
+    } catch (error) {
+      console.log(error);
+      alert(error.message);
+    }
+  };
+
+  const { username, email, password, repeatPassword } = inputs;
+
+  return (
+    <form onSubmit={onSubmitHandler}>
+      <input
+        type="text"
+        name="username"
+        placeholder="Имя пользователя"
+        autoFocus
+        onChange={onChangeHandler}
+        value={username}
+      />
+      <input
+        type="email"
+        name="email"
+        placeholder="Эл. почта"
+        autoFocus
+        onChange={onChangeHandler}
+        value={email}
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="Пароль"
+        autoFocus
+        onChange={onChangeHandler}
+        value={password}
+      />
+      <input
+        type="password"
+        name="repeatPassword"
+        placeholder="Повторите пароль"
+        autoFocus
+        onChange={onChangeHandler}
+        value={repeatPassword}
+      />
+
+      <button type="submit">Регистрация</button>
+    </form>
+  );
 }
