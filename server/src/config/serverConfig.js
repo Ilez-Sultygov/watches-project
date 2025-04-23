c350132a5fab04ae89b3ab375610800bb210941d
GlebUrv <punkhoygleb@mail.ru>
2025-04-23 20:27:14 +0500

    auth added cookies (lecture 221)

--- server/src/config/serverConfig.js
+++ server/src/config/serverConfig.js
@@ -1,16 +1,18 @@
-const express = require('express');
-const path = require('path');
-const morgan = require('morgan');
-const removeHTTPHeader = require('../middleware/removeHeader');
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const removeHTTPHeader = require("../middleware/removeHeader");
 
 const serverConfig = (app) => {
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
-  app.use(morgan('dev'));
+  app.use(morgan("dev"));
+  app.use(cookieParser());
   app.use(removeHTTPHeader);
   app.use(
-    '/static/images',
-    express.static(path.resolve(__dirname, '..', 'public', 'images')),
+    "/static/images",
+    express.static(path.resolve(__dirname, "..", "public", "images"))
   );
 };
 
