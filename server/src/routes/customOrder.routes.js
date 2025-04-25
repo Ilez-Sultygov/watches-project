67e7fc794fd78d7553200bb33566340a91ea98fd
TatianaVoznichik <kim_t.a@bk.ru>
2025-04-25 19:20:14 +1000

    adminPage is ready without DELETE

--- server/src/routes/customOrder.routes.js
+++ server/src/routes/customOrder.routes.js
@@ -1,15 +1,12 @@
 const router = require("express").Router();
 const multer = require("multer");
 const CustomOrdersController = require("../controllers/CustomOrders.controller");
-const { upload } = require("../middleware/upload");
+const { uploadUser} = require("../middleware/upload");
 const verifyAccessToken = require("../middleware/verifyAccessToken");
 
-router.post(
-  "/customOrder",
-  upload.single("image"),
-  CustomOrdersController.CustomOrder
-);
 router.get("/userCustomOrders/:id", CustomOrdersController.getCustomOrders);
 
-router.post('/customOrder',verifyAccessToken, upload.single('image'), CustomOrdersController.CustomOrder) 
+router.post('/customOrder', verifyAccessToken, uploadUser.single('image'), CustomOrdersController.CustomOrder) 
+
+
 module.exports = router;
